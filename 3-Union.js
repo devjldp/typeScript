8efe42ef4ebe1c36dc6dd02e232c30238e23672d
JoseDev <joseluisdp297@gmail.com>
2024-07-15 13:53:50 +0100

    unions

---
+++ 3-Union.js
@@ -0,0 +1,8 @@
+// ======================= Union and Literals types =============================
+// Union when we have two or more types.
+// Narrowing when we reduce types not to be one or more.
+// Unio => symbol |
+myVariable2 = 'house';
+console.log(myVariable2);
+myVariable2 = 5;
+console.log(myVariable2);
---
+++ 3-Union.ts
@@ -0,0 +1,12 @@
+// ======================= Union and Literals types =============================
+
+// Union when we have two or more types.
+// Narrowing when we reduce types not to be one or more.
+
+// Unio => symbol |
+
+let myVariable2: string | number = 'house';
+console.log(myVariable2)
+myVariable2 = 5
+console.log(myVariable2)
+
