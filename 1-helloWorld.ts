edb31a22121c78f1cafd89874a51d857957debce
JoseDev <joseluisdp297@gmail.com>
2024-07-15 13:53:23 +0100

    hello world in ts

---
+++ 1-helloWorld.ts
@@ -0,0 +1,8 @@
+// We can use JavaScript syntax
+console.log("Hello JavaScript")
+
+// Variables:
+
+let myString = "Hello"
+// myString = 5 => This is an error we can't assign type number to type string
+console.log(myString)
